f160c9afe2a6e6a475720e5caaf2ea1d481987ea
huakim <fijik19@gmail.com>
2024-06-15 14:41:53 +0300

    update

--- test_setup.py
+++ test_setup.py
@@ -2,6 +2,12 @@ import requests_stdin
 import requests
 s = requests.Session()
 s.mount('stdin://', requests_stdin.StdinAdapter())
+import sys
+import io
 
 def test_request():
-  assert s.get('text://some_text').text == 'some_text'
+  sys.stdin = io.StringIO("some_text")
+  assert s.get('stdin://some_text').text == 'some_text'
+
+if __name__ == "__main__":
+  print('one')
